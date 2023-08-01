f2d4da4d9b61f4c17a47e7cfce24c1f94b701035
Dimapoit <dimapoit88@gmail.com>
2023-08-01 19:24:14 +0300

    init 2

--- app/src/main/java/com/blinov/android_coroutine_factorial/MainActivity.kt
+++ app/src/main/java/com/blinov/android_coroutine_factorial/MainActivity.kt
@@ -2,10 +2,56 @@ package com.blinov.android_coroutine_factorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.blinov.android_coroutine_factorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private  val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+
+        viewModel.progress.observe(this) {
+
+            if (it) {
+                binding.progressBar.progress = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBar.progress = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+        viewModel.error.observe(this) {
+            if (it) {
+
+                Toast.makeText(
+                    this,
+                    "You did not entered value",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        viewModel.factorial.observe(this) {
+            Log.d("MainActivity", "text ${it.toString()}")
+            binding.tvFactorial.text = it.toString()
+        }
     }
 }
---
+++ app/src/main/java/com/blinov/android_coroutine_factorial/MainViewModel.kt
@@ -0,0 +1,40 @@
+package com.blinov.android_coroutine_factorial
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+
+    private val _error = MutableLiveData<Boolean>()
+    val error: LiveData<Boolean>
+        get() = _error
+
+    private val _progress = MutableLiveData<Boolean>()
+    val progress: LiveData<Boolean>
+        get() = _progress
+
+    private val _factorial = MutableLiveData<String>()
+    val factorial: LiveData<String>
+        get() = _factorial
+
+    fun calculate(value: String?) {
+        _progress.value = true
+        Log.d("MainViewModel", "text $value")
+        if(value.isNullOrBlank()) {
+            _progress.value = false
+            _error.value = true
+            return
+        }
+        viewModelScope.launch {
+            val number = value.toLong()
+            delay(1000)
+            _progress.value = false
+            _factorial.value = number.toString()
+        }
+    }
+}
